d9f643ca5c031d15009044b839712868940709dc
pcPersoTravaill <640poto@gmail.com>
2024-12-20 16:05:49 +0100

    first commit

---
+++ js/personnages.js
@@ -0,0 +1,70 @@
+export const heros = [
+    {
+        nom: "Guerrier",
+        description: "Fort et courageux, spécialisé dans les combats rapprochés.",
+        image: "./images/guerrier.webp",
+        stats: {
+            vie: 150,
+            attaque: 20,
+            endurance: 15,
+            mana: 0
+        },
+        inventaire: ["Épée", "Bouclier"]
+    },
+    {
+        nom: "Mage",
+        description: "Maître des arcanes, utilise la magie pour infliger des dégâts.",
+        image: "./images/mage.webp",
+        stats: {
+            vie: 100,
+            attaque: 15,
+            endurance: 5,
+            mana: 50
+        },
+        inventaire: ["Bâton magique", "Parchemin de feu"]
+    },
+    {
+        nom: "Archer",
+        description: "Rapide et précis, inflige des dégâts à distance.",
+        image: "./images/archer.webp",
+        stats: {
+            vie: 120,
+            attaque: 18,
+            endurance: 10,
+            mana: 10
+        },
+        inventaire: ["Arc", "Flèches"]
+    }
+];
+
+
+
+const ennemis = [
+    {
+        nom: "Gobelin",
+        description: "Petite créature verte et sournoise, armée d'un couteau rouillé.",
+        image: "./images/gobelin.jpg",
+        stats: {
+            vie: 50,
+            attaque: 10
+        }
+    },
+    {
+        nom: "Squelette",
+        description: "Ancien guerrier revenu à la vie, armé d'une épée rouillée.",
+        image: "./images/squelette.jpg",
+        stats: {
+            vie: 80,
+            attaque: 15
+        }
+    },
+    {
+        nom: "Orc",
+        description: "Créature massive et brutale, armée d'une hache énorme.",
+        image: "./images/orc.jpg",
+        stats: {
+            vie: 100,
+            attaque: 20
+        }
+    }
+];
